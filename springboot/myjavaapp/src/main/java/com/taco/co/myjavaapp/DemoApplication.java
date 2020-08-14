cbc46ab39c5a5a68cb66321fee142aac9014d0f8
yashs98 <sinhayash98@gmail.com>
2020-08-14 17:35:03 -0600

    Initialized project.

---
+++ springboot/myjavaapp/src/main/java/com/taco/co/myjavaapp/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.taco.co.myjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
